478fed51f7c5af1040156c60fadef59d3c4b2dc4
KippiPippi <159530894+KippiPippi@users.noreply.github.com>
2024-02-15 09:30:54 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -56,7 +56,7 @@ module.exports = {
            { name: '🎵 Now Playing', value: 'Display the currently playing song information' },
           { name: '🔊 Volume', value: 'Adjust the music volume [ hearing at high volumes is risky ]' },
         ) 
-       .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
+       .setImage('https://media.discordapp.net/attachments/708816457101869078/1173653935416950805/ezgif.com-gif-maker_2.gif?ex=65dcb5e7&is=65ca40e7&hm=2d6526237ff25c747dc00653cd033b87857c794dabe2e93f046c67df69671365&')
       const button1 = new ButtonBuilder()
         .setLabel('YouTube')
         .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
